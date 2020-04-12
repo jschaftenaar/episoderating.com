321d4c5bd570bb69c9241877264cfb6a84b3a26e
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2020-04-11 23:37:30 -0500

    Update cfdeploy.sh

--- cfdeploy.sh
+++ cfdeploy.sh
@@ -3,12 +3,8 @@
 echo "The branch is:"
 echo "$1"
 
-echo "$GITHUB_REF"
-echo "$GITHUB_HEAD_REF"
-echo "$GITHUB_BASE_REF"
-
 #release/paigebot
-if [ "$1" == "release/v1" ]; then
+if [ "$1" == "refs/heads/release/v1" ]; then
   curl -sL https://ibm.biz/idt-installer | bash
   ibmcloud cf install
   ibmcloud login -r us-south --apikey=$2
