171fc52b6afa0610b26ea4332b03537207677ec5
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2020-04-11 23:33:53 -0500

    Update cfdeploy.sh

--- cfdeploy.sh
+++ cfdeploy.sh
@@ -3,6 +3,10 @@
 echo "The branch is:"
 echo "$1"
 
+echo "$GITHUB_BASE_REF"
+echo "$GITHUB_HEAD_REF"
+echo "$GITHUB_BASE_REF"
+
 #release/paigebot
 if [ "$1" == "release/v1" ]; then
   curl -sL https://ibm.biz/idt-installer | bash
