6c0489b7efc9212f72544e067893da98b2a8e1a8
Jeroen Schaftenaar <4943036+jschaftenaar@users.noreply.github.com>
2020-12-11 14:04:58 -0600

    revert echo of id_rsa

--- dokkudeploy.sh
+++ dokkudeploy.sh
@@ -7,10 +7,7 @@ if [ "$1" == "refs/heads/release/v1" ]; then
   mkdir -p ~/.ssh
   echo "$ID_RSA" > ~/.ssh/id_rsa
   echo "$ID_RSA_PUB" > ~/.ssh/id_rsa.pub
-  cat ~/.ssh/id_rsa
-  cat ~/.ssh/id_rsa.pub
   chmod 700 -R ~/.ssh
-  ssh-keygen -l -f ~/.ssh/id_rsa.pub
   git fetch --unshallow
   ssh-keyscan -t rsa host.416.amsdfw.us >> ~/.ssh/known_hosts
   git remote add dokku dokku@host.416.amsdfw.us:episoderating
